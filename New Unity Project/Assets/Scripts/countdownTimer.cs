5f987b7cd6bdfad380e73b77c81ce661460b73e5
Dylan446 <34541377+Dylan446@users.noreply.github.com>
2021-08-23 01:39:51 +0100

    Alpha push

---
+++ New Unity Project/Assets/Scripts/countdownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class countdownTimer : MonoBehaviour
+{
+    public GameObject timeText;
+    float currentTime = 0;
+    float startingTime = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        timeText.GetComponent<Text>().text = "TIME: " + currentTime;
+
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/itemPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class itemPickup : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int scoreCount;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && scoreCount == 3)
+        {
+            print("Level Complete!");
+            SceneManager.LoadScene("Level 2");
+        }
+
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Key picked up");
+
+            scoreCount++;
+            Destroy(gameObject);
+            Update();
+        }
+        void Update()
+        {
+            print(+scoreCount);
+            scoreText.GetComponent<Text>().text = "KEYS: " + scoreCount;
+        }
+    }
+}
